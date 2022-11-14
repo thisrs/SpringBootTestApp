35e8823782bdb87a5813334b91d2e1e1a74ee659
Roman <leavers@mail.ru>
2022-11-14 22:30:36 +0300

    add spring security

---
+++ src/main/java/com/example/springboottestapp/domain/Message.java
@@ -0,0 +1,27 @@
+package com.example.springboottestapp.domain;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String text;
+    private String tag;
+
+    public Message(String text, String tag) {
+        this.text = text;
+        this.tag = tag;
+    }
+
+    public Message() {
+
+    }
+}
---
+++ src/main/java/com/example/springboottestapp/domain/Role.java
@@ -0,0 +1,4 @@
+package com.example.springboottestapp.domain;
+
+public enum Role {
+}
